700fea49f72b6a410a262fe9ebf69eeee6b97f8c
kTojo <hoseiko1@icloud.com>
2021-06-27 16:19:56 +0900

    bgn ver 1.0.0

---
+++ beginner/Sample202/Sample202/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Sample202
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;  //変数の宣言
+            int b = 3;  //初期化と代入
+            int add, sub;  //複数の宣言
+            double avg;  //int以外の変数
+            a = 6;  //代入
+            add = a + b;  //a,bの和
+            sub = a - b;  //a,bの差
+            avg = (a + b) / 2.0;  //a,bの平均
+            Console.WriteLine("{0} + {1} = {2}", a, b, add);
+            Console.WriteLine("{0} - {1} = {2}", a, b, sub);
+            Console.WriteLine("{0} と {1} の平均 {2}", a, b, avg);
+
+        }
+    }
+}
---
+++ beginner/Sample206/Sample206/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Sample206
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int NUMBER = 100;
+            const string STRING = "hoge";
+            Console.WriteLine(NUMBER);
+            Console.WriteLine(STRING);
+            //  constがついた変数は値を変えられない
+        }
+    }
+}
